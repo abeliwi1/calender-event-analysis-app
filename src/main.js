2c4c592dcda4df0b5ea081ce7a6df8eca15d2468
abeliwi1 <abeliwi1@jh.edu>
2026-01-03 18:30:22 -0500

    Delete src directory

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import "./style.css";
-
-const app = document.getElementById("app");
-app.innerHTML = `<div class="flex items-center justify-center h-screen">
-  <h1 class="text-5x1">Hello, FSJS!</h1>
-</div>`;
